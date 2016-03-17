9d317b8c47c3d2c7f67834e2d507a5aaba86dcae
mao jiayin <maojiayin@gmail.com>
2016-03-16 22:42:44 -0400

    Task 1 (a). Allow user to type input parameters.

--- test.cpp
+++ test.cpp
@@ -1,8 +1,25 @@
+#include <iostream>
 #include "trimatrix.h"
 
 int main()
 {
+  int L, N_x, T, N_t, alpha;
+  std::cout << "Please provide L: ";
+  std::cin >> L;
+
+  std::cout << "Please provide Nx: ";
+  std::cin >> N_x;
+
+  std::cout << "Please provide T: ";
+  std::cin >> T;
+
+  std::cout << "Please provide Nt: ";
+  std::cin >> N_t;
+
+  std::cout << "Please provide alpha: ";
+  std::cin >> alpha;
+  
   TriMatrix triMatrix;
-  triMatrix.setCount(10);
+  triMatrix.setCount(L);
   triMatrix.print();
 }
